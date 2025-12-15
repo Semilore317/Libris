aab12aeff8fb518858fabf0059a6ef05e7218462
Abraham Bankole <semi.banks.317@gmail.com>
2025-12-15 18:25:28 +0100

    added test suite

--- src/test/java/com/example/libris/LibrisApplicationTests.java
+++ src/test/java/com/example/libris/LibrisApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.libris;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LibrisApplicationTests {
 
     @Test
